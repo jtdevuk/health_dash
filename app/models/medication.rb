2264f083f3e44b202d1f0c696dc07c4522bc20aa
John Tweeddale <j.tweeddale@outlook.com>
2021-03-02 22:23:09 +0000

    add dependent: :destroy to Medication model to allow medication delete

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,5 +1,5 @@
 class Medication < ApplicationRecord
-  has_many :user_medications
+  has_many :user_medications, :dependent => :destroy
   has_many :users, through: :user_medications
   validates :name, :description, presence: true
 end

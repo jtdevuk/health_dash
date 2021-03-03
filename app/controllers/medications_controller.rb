a85e94caeb267c2f06b4b3a12d3a197a773f74e2
John Tweeddale <j.tweeddale@outlook.com>
2021-03-03 19:55:20 +0000

    added create action for medications

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -8,4 +8,22 @@ class MedicationsController < ApplicationController
   def new
     @med = Medication.new
   end
+
+  def create
+    @med = Medication.new(medication_params)
+    @med.user_id = current_user.id
+
+    if @med.save
+      redirect_to medications_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def medication_params
+    params.require(:medication).permit(:name, :description, :remaining, :user_id)
+  end
+
 end

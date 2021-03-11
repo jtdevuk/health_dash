0c97ea67afe87504172d612761b69954edf82b1b
John Tweeddale <j.tweeddale@outlook.com>
2021-03-11 02:51:59 +0000

    added display for user appointments in calendar and changed calendar CSS to default

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,10 @@
 class AppointmentsController < ApplicationController
   def index
     @new_appointment = Appointment.new
-    @appointments = Appointment.all
+    # @appointments = Appointment.all
+
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @appointments = Appointment.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 
   def create
@@ -18,7 +21,7 @@ class AppointmentsController < ApplicationController
   private
 
   def appointment_params
-    params.require(:appointment).permit(:name, :location, :date)
+    params.require(:appointment).permit(:name, :location, :start_time)
   end
 
 end

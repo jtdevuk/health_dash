1a8f8a8bf375ec80f46533bd52bec07c612c4b1c
John Tweeddale <j.tweeddale@outlook.com>
2021-03-31 15:27:48 +0100

    only display close button for record attachement when attachment present

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -22,19 +22,6 @@ class RecordsController < ApplicationController
     @record.user_id = current_user.id    
     @record.letter.attach(params[:record][:letter])
 
-    # if params[:other][:link_to_calendar] == "1"
-    #   year = params["record"]["appointment_date(1i)"].to_i
-    #   month = params["record"]["appointment_date(2i)"].to_i
-    #   day = params["record"]["appointment_date(3i)"].to_i
-    #   appointment = Appointment.create(name: @record.name,
-    #                                     location: @record.description,
-    #                                     start_time: DateTime.new(year, month, day),
-    #                                     record_id: @record.id,
-    #                                     user_id: current_user.id)
-    #   current_user.appointments << appointment
-    #   @record.appointment = appointment
-    # end
-
     add_to_calendar if params[:other][:link_to_calendar] == "1"
     
     if @record.save
@@ -46,6 +33,7 @@ class RecordsController < ApplicationController
 
   def update    
     if @record.update(record_params)
+      @record.letter.attach(params[:record][:letter])
       redirect_to records_path
     else
       render "edit"

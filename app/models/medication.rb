531676c6279fe4853c949c0e8b7de5771577d42b
John Tweeddale <j.tweeddale@outlook.com>
2021-04-01 16:55:34 +0100

    added method to Medication to keep track of how much remains after time has passed

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -3,6 +3,10 @@ class Medication < ApplicationRecord
   validates :name, :description, presence: true
 
   def supply(med)
-    "#{med.remaining / med.dose} days"
+    last_updated = med.updated_at.to_date
+    today = Date.today
+    days_passed = (today - last_updated).to_i + 1
+    med_used = med.dose * days_passed
+    "#{med.remaining / med_used} days"
   end
 end

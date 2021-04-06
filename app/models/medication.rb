dea4447edd8fb65e8231334cb438dfef14c03089
John Tweeddale <j.tweeddale@outlook.com>
2021-04-06 10:47:41 +0100

    changed medication remaining and supply calculations and display in medications table

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -2,12 +2,24 @@ class Medication < ApplicationRecord
   belongs_to :user
   validates :name, :description, presence: true
 
-  def supply(med)
+  def calc_remaining(med)
+    med_taken = med.dose * days_passed(med)
+    med.remaining -= med_taken
+  end
+
+  def calc_supply(med)    
+      remaining = med.remaining / med.dose
+      case
+      when remaining == 1 then "#{remaining} day"
+      when remaining <= 0 then "None remaining"
+      when remaining > 1 then "#{remaining} days"
+      end
+  end
+
+  private
+
+  def days_passed(med)
     last_updated = med.updated_at.to_date
-    today = Date.today
-    days_passed = (today - last_updated).to_i + 1
-    med_used = med.dose * days_passed
-    remaining = med.remaining / med_used
-    remaining > 1 ? "#{remaining} days" : "#{remaining} day"
+    days_passed = (Date.today - last_updated).to_i
   end
 end

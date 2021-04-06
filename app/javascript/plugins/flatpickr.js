1234fdc2713026ad6a257f39bacd8a692ea3f189
John Tweeddale <j.tweeddale@outlook.com>
2021-04-06 11:47:55 +0100

    styled filter by date range on records index page

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,6 +2,7 @@ import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
   flatpickr(".date-time-picker", {enableTime: true});
+  flatpickr(".date-picker", {});
 }
 
 export { initFlatpickr };

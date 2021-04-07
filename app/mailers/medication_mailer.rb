a7f3c6d347fd12f819a2e34cece68f8279520daf
John Tweeddale <j.tweeddale@outlook.com>
2021-04-07 14:54:45 +0100

    generated MedicationMailer

---
+++ app/mailers/medication_mailer.rb
@@ -0,0 +1,2 @@
+class MedicationMailer < ApplicationMailer
+end
---
+++ spec/mailers/previews/medication_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/medication_mailer
+class MedicationMailerPreview < ActionMailer::Preview
+
+end

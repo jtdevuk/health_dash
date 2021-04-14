2d398079a73a8ea8633a05cd7d6a4288f919bf20
John Tweeddale <j.tweeddale@outlook.com>
2021-04-14 11:58:50 +0100

    added record letter attachment to appointment reminder

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -2,6 +2,13 @@ class AppointmentMailer < ApplicationMailer
   def appointment_reminder
     @user = params[:user]
     @app = params[:app]
+    if params[:app].record.present? && params[:app].record.letter.attached?
+      @attachment = params[:app].record.letter    
+      attachments['Appointment.pdf'] = {
+      mime_type: @attachment.blob.content_type,
+      content: @attachment.blob.download
+      }
+    end
     mail(to: 'j.tweeddale@outlook.com', subject: 'Upcoming appointment reminder')
   end
 end

d17b7ee4e3643abbfc6ee4b02ff9096becbdef48
John Tweeddale <j.tweeddale@outlook.com>
2021-05-29 16:57:39 +0100

    switched to PG ready for production

--- db/migrate/20210306184851_add_foreign_key_user_id_to_medications.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyUserIdToMedications < ActiveRecord::Migration[6.0]
-  def change
-    add_foreign_key :medications, :users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_05_29_121010) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -37,7 +40,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_121010) do
     t.string "name"
     t.string "location"
     t.datetime "start_time"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
@@ -48,7 +51,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_121010) do
     t.string "name"
     t.text "description"
     t.integer "remaining"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "dose"
@@ -57,7 +60,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_121010) do
   end
 
   create_table "records", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "name"
     t.text "description"
     t.string "category"
@@ -81,6 +84,5 @@ ActiveRecord::Schema.define(version: 2021_05_29_121010) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "appointments", "users"
-  add_foreign_key "medications", "users"
   add_foreign_key "medications", "users"
 end
